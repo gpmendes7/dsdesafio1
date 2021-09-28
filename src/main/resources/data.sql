ab94347121dd9afd4de0101d6e72822aa33e2093
gabriel7mendes <andrewsseven@gmail.com>
2021-09-28 17:41:33 -0300

    Database seeding

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Mario', '459.858.150-64', 3500.50, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carolina de Azevedo', '842.080.520-31', 2000, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anthonio Sierra de La Cruz', '280.748.660-68', 1775.65, NOW(), 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Talita Medeiros', '502.867.870-75', 7000.00, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Larissa dos Santos', '710.704.130-46', 600.00, NOW(), 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Joel Carvalho de Albuquerque', '564.875.300-42', 1900.00, NOW(), 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gael Saboya Filho', '557.823.670-75', 2390.00, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Vinicius Vieira', '135.542.560-30', 4750.00, NOW(), 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Paloma Rabello de Amorim', '944.329.820-12', 5700.00, NOW(), 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Aisha Khumalo', '630.473.460-38', 800.00, NOW(), 3);
