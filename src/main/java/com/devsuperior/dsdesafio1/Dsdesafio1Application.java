d0bbbd0e09a807f927d706fee1872be0a997cbeb
gabriel7mendes <andrewsseven@gmail.com>
2021-09-28 15:54:05 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dsdesafio1/Dsdesafio1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafio1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsdesafio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsdesafio1Application.class, args);
+	}
+
+}
